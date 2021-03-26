6dafc5343ca5e0e5875ed994a5252cb5677d3d0d
Julia <ulia.nikiforova124@gmail.com>
2021-03-26 20:12:30 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -16,20 +16,59 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 })
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+// slick-slider:
+
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         /* adaptiveHeight: true, */
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+//   });
+
+// tiny-slider (without jQuery):
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+//   });
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');  
+// });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');  
+// });
+
+// owl.carousel slider:
+
+var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      items: 1,
+      nav: false,
+      loop: true,
+      autoHeight: true
   });
+  // Go to the next item
+  $('.next').click(function() {
+      owl.trigger('next.owl.carousel');
+  })
+  // Go to the previous item
+  $('.prev').click(function() {
+      owl.trigger('prev.owl.carousel');
+  })
