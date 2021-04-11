0f886de7760e2089b396036685c07cc5e68e335b
Julia <ulia.nikiforova124@gmail.com>
2021-04-11 10:54:05 +0300

    add catalog and feedback

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,50 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+$(document).ready(function(){
+    // owl.carousel slider:
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+        nav: false,
+        loop: true,
+        autoHeight: false,
+        responsive:{
+            320:{
+                dots: true,
+                dotsEach: true, 
+            }
+        }
+    });
+  // Go to the next item
+    $('.next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+  // Go to the previous item
+    $('.prev').click(function() {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });   
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+
 // slick-slider:
 
 // $(document).ready(function(){
@@ -54,21 +98,3 @@ window.addEventListener('DOMContentLoaded', () => {
 // document.querySelector('.next').addEventListener('click', function () {
 //     slider.goTo('next');  
 // });
-
-// owl.carousel slider:
-
-var owl = $('.owl-carousel');
-  owl.owlCarousel({
-      items: 1,
-      nav: false,
-      loop: true,
-      autoHeight: true
-  });
-  // Go to the next item
-  $('.next').click(function() {
-      owl.trigger('next.owl.carousel');
-  })
-  // Go to the previous item
-  $('.prev').click(function() {
-      owl.trigger('prev.owl.carousel');
-  })
